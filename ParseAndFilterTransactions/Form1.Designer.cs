7086eaf8bbeb25eb8feaed09927e7b205b148fd8
Israel DiPeri <israeldiperi@hotmail.com>
2024-09-15 16:10:41 -0400

    code clean up

--- ParseAndFilterTransactions/Form1.Designer.cs
+++ ParseAndFilterTransactions/Form1.Designer.cs
@@ -28,14 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox_Bank = new System.Windows.Forms.ComboBox();
-            this.textBox_RootFolder = new System.Windows.Forms.TextBox();
-            this.buttonSelectSourceFile = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.textBoxFileName = new System.Windows.Forms.TextBox();
-            this.comboBox_Filter = new System.Windows.Forms.ComboBox();
-            this.listBoxFilterKeywords = new System.Windows.Forms.ListBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox_LoadFiles = new System.Windows.Forms.GroupBox();
             this.loadFilesCtrl1 = new ParseAndFilterTransactions.LoadFilesCtrl();
             this.groupBox_FilterTranx = new System.Windows.Forms.GroupBox();
@@ -44,92 +37,15 @@
             this.summarizeTranxCtrl1 = new ParseAndFilterTransactions.SummarizeTranxCtrl();
             this.groupBox_Save = new System.Windows.Forms.GroupBox();
             this.saveSummaryDataCtrl1 = new ParseAndFilterTransactions.SaveSummaryDataCtrl();
-            this.groupBox1.SuspendLayout();
             this.groupBox_LoadFiles.SuspendLayout();
             this.groupBox_FilterTranx.SuspendLayout();
             this.groupBox_SummarizeTranx.SuspendLayout();
             this.groupBox_Save.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBox_Bank
-            // 
-            this.comboBox_Bank.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox_Bank.FormattingEnabled = true;
-            this.comboBox_Bank.Location = new System.Drawing.Point(8, 105);
-            this.comboBox_Bank.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.comboBox_Bank.Name = "comboBox_Bank";
-            this.comboBox_Bank.Size = new System.Drawing.Size(195, 24);
-            this.comboBox_Bank.TabIndex = 2;
-            this.comboBox_Bank.SelectedIndexChanged += new System.EventHandler(this.comboBox_Bank_SelectedIndexChanged);
-            // 
-            // textBox_RootFolder
-            // 
-            this.textBox_RootFolder.Location = new System.Drawing.Point(231, 28);
-            this.textBox_RootFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox_RootFolder.Name = "textBox_RootFolder";
-            this.textBox_RootFolder.Size = new System.Drawing.Size(631, 22);
-            this.textBox_RootFolder.TabIndex = 3;
-            // 
-            // buttonSelectSourceFile
-            // 
-            this.buttonSelectSourceFile.Location = new System.Drawing.Point(231, 60);
-            this.buttonSelectSourceFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.buttonSelectSourceFile.Name = "buttonSelectSourceFile";
-            this.buttonSelectSourceFile.Size = new System.Drawing.Size(51, 34);
-            this.buttonSelectSourceFile.TabIndex = 4;
-            this.buttonSelectSourceFile.Text = "...";
-            this.buttonSelectSourceFile.UseVisualStyleBackColor = true;
-            this.buttonSelectSourceFile.Click += new System.EventHandler(this.buttonSelectSourceFile_Click);
-            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // textBoxFileName
-            // 
-            this.textBoxFileName.Location = new System.Drawing.Point(289, 66);
-            this.textBoxFileName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBoxFileName.Name = "textBoxFileName";
-            this.textBoxFileName.Size = new System.Drawing.Size(573, 22);
-            this.textBoxFileName.TabIndex = 5;
-            // 
-            // comboBox_Filter
-            // 
-            this.comboBox_Filter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox_Filter.FormattingEnabled = true;
-            this.comboBox_Filter.Location = new System.Drawing.Point(8, 138);
-            this.comboBox_Filter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.comboBox_Filter.Name = "comboBox_Filter";
-            this.comboBox_Filter.Size = new System.Drawing.Size(195, 24);
-            this.comboBox_Filter.TabIndex = 6;
-            this.comboBox_Filter.SelectedIndexChanged += new System.EventHandler(this.comboBox_Filter_SelectedIndexChanged);
-            // 
-            // listBoxFilterKeywords
-            // 
-            this.listBoxFilterKeywords.FormattingEnabled = true;
-            this.listBoxFilterKeywords.ItemHeight = 16;
-            this.listBoxFilterKeywords.Location = new System.Drawing.Point(231, 117);
-            this.listBoxFilterKeywords.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.listBoxFilterKeywords.Name = "listBoxFilterKeywords";
-            this.listBoxFilterKeywords.Size = new System.Drawing.Size(148, 68);
-            this.listBoxFilterKeywords.TabIndex = 7;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.listBoxFilterKeywords);
-            this.groupBox1.Controls.Add(this.comboBox_Filter);
-            this.groupBox1.Controls.Add(this.comboBox_Bank);
-            this.groupBox1.Controls.Add(this.textBoxFileName);
-            this.groupBox1.Controls.Add(this.textBox_RootFolder);
-            this.groupBox1.Controls.Add(this.buttonSelectSourceFile);
-            this.groupBox1.Location = new System.Drawing.Point(3, 468);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(880, 213);
-            this.groupBox1.TabIndex = 8;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Obsolete";
             // 
             // groupBox_LoadFiles
             // 
@@ -137,9 +53,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox_LoadFiles.Controls.Add(this.loadFilesCtrl1);
             this.groupBox_LoadFiles.Location = new System.Drawing.Point(16, 22);
-            this.groupBox_LoadFiles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_LoadFiles.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox_LoadFiles.Name = "groupBox_LoadFiles";
-            this.groupBox_LoadFiles.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_LoadFiles.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox_LoadFiles.Size = new System.Drawing.Size(999, 220);
             this.groupBox_LoadFiles.TabIndex = 9;
             this.groupBox_LoadFiles.TabStop = false;
@@ -151,7 +67,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.loadFilesCtrl1.Location = new System.Drawing.Point(8, 23);
-            this.loadFilesCtrl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.loadFilesCtrl1.Margin = new System.Windows.Forms.Padding(5);
             this.loadFilesCtrl1.Name = "loadFilesCtrl1";
             this.loadFilesCtrl1.Size = new System.Drawing.Size(983, 190);
             this.loadFilesCtrl1.TabIndex = 0;
@@ -163,9 +79,9 @@
             this.groupBox_FilterTranx.Controls.Add(this.filterTranxCtrl1);
             this.groupBox_FilterTranx.Enabled = false;
             this.groupBox_FilterTranx.Location = new System.Drawing.Point(16, 261);
-            this.groupBox_FilterTranx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_FilterTranx.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox_FilterTranx.Name = "groupBox_FilterTranx";
-            this.groupBox_FilterTranx.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_FilterTranx.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox_FilterTranx.Size = new System.Drawing.Size(999, 206);
             this.groupBox_FilterTranx.TabIndex = 10;
             this.groupBox_FilterTranx.TabStop = false;
@@ -177,7 +93,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.filterTranxCtrl1.Location = new System.Drawing.Point(8, 23);
-            this.filterTranxCtrl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.filterTranxCtrl1.Margin = new System.Windows.Forms.Padding(5);
             this.filterTranxCtrl1.Name = "filterTranxCtrl1";
             this.filterTranxCtrl1.Size = new System.Drawing.Size(983, 175);
             this.filterTranxCtrl1.TabIndex = 0;
@@ -189,9 +105,9 @@
             this.groupBox_SummarizeTranx.Controls.Add(this.summarizeTranxCtrl1);
             this.groupBox_SummarizeTranx.Enabled = false;
             this.groupBox_SummarizeTranx.Location = new System.Drawing.Point(16, 474);
-            this.groupBox_SummarizeTranx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_SummarizeTranx.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox_SummarizeTranx.Name = "groupBox_SummarizeTranx";
-            this.groupBox_SummarizeTranx.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_SummarizeTranx.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox_SummarizeTranx.Size = new System.Drawing.Size(999, 127);
             this.groupBox_SummarizeTranx.TabIndex = 12;
             this.groupBox_SummarizeTranx.TabStop = false;
@@ -203,7 +119,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.summarizeTranxCtrl1.Location = new System.Drawing.Point(8, 23);
-            this.summarizeTranxCtrl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.summarizeTranxCtrl1.Margin = new System.Windows.Forms.Padding(5);
             this.summarizeTranxCtrl1.Name = "summarizeTranxCtrl1";
             this.summarizeTranxCtrl1.Size = new System.Drawing.Size(983, 96);
             this.summarizeTranxCtrl1.TabIndex = 11;
@@ -213,9 +129,9 @@
             this.groupBox_Save.Controls.Add(this.saveSummaryDataCtrl1);
             this.groupBox_Save.Enabled = false;
             this.groupBox_Save.Location = new System.Drawing.Point(16, 608);
-            this.groupBox_Save.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_Save.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox_Save.Name = "groupBox_Save";
-            this.groupBox_Save.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_Save.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox_Save.Size = new System.Drawing.Size(987, 116);
             this.groupBox_Save.TabIndex = 13;
             this.groupBox_Save.TabStop = false;
@@ -227,7 +143,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.saveSummaryDataCtrl1.Location = new System.Drawing.Point(8, 23);
-            this.saveSummaryDataCtrl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.saveSummaryDataCtrl1.Margin = new System.Windows.Forms.Padding(5);
             this.saveSummaryDataCtrl1.Name = "saveSummaryDataCtrl1";
             this.saveSummaryDataCtrl1.Size = new System.Drawing.Size(971, 85);
             this.saveSummaryDataCtrl1.TabIndex = 0;
@@ -237,16 +153,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1031, 750);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox_Save);
             this.Controls.Add(this.groupBox_SummarizeTranx);
             this.Controls.Add(this.groupBox_FilterTranx);
             this.Controls.Add(this.groupBox_LoadFiles);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.groupBox_LoadFiles.ResumeLayout(false);
             this.groupBox_FilterTranx.ResumeLayout(false);
             this.groupBox_SummarizeTranx.ResumeLayout(false);
@@ -256,14 +169,7 @@
         }
 
         #endregion
-        private System.Windows.Forms.ComboBox comboBox_Bank;
-        private System.Windows.Forms.TextBox textBox_RootFolder;
-        private System.Windows.Forms.Button buttonSelectSourceFile;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.TextBox textBoxFileName;
-        private System.Windows.Forms.ComboBox comboBox_Filter;
-        private System.Windows.Forms.ListBox listBoxFilterKeywords;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox_LoadFiles;
         private LoadFilesCtrl loadFilesCtrl1;
         private System.Windows.Forms.GroupBox groupBox_FilterTranx;
--- ParseAndFilterTransactions/Form1.cs
+++ ParseAndFilterTransactions/Form1.cs
@@ -18,40 +18,6 @@ namespace ParseAndFilterTransactions
             InitializeComponent();
 
             ParseTransactions.OnChanged += ParseTransactions_OnChanged;
-
-            m_RootFolders = new Dictionary<DataFormat, string>();
-            //C:\Users\israe\Documents\Financial\Bank of America\Transactions
-            m_RootFolders.Add(DataFormat.BankOfAmerica, @"C:\Users\israe\Documents\Financial\Bank of America\Transactions");
-            m_RootFolders.Add(DataFormat.CapitolOne360_Rev0, @"C:\Users\israe\Documents\Financial\Capital One 360\Transactions");
-
-            m_FilterGroupKeywords = new Dictionary<FilterGroup, List<string>>();
-            List<string> FoodFilterList = new List<string>();
-            FoodFilterList.Add("TRADER JOE");
-            FoodFilterList.Add("WHOLEFDS");
-            FoodFilterList.Add("SHAW S MARKET");
-            m_FilterGroupKeywords.Add(FilterGroup.Food, FoodFilterList);
-
-            List<string> UtilityList = new List<string>();
-            UtilityList.Add("TARGET");
-            UtilityList.Add("CVS PHARMACY");
-            UtilityList.Add("WALGREENS");
-            m_FilterGroupKeywords.Add(FilterGroup.Utility, UtilityList);
-
-            List<string> GasList = new List<string>();
-            GasList.Add("SUNOCO");
-            GasList.Add("A-Z SERVICE");
-            GasList.Add("SHELL OIL");
-            GasList.Add("GULF OIL");
-            m_FilterGroupKeywords.Add(FilterGroup.Gas, GasList);
-
-            //comboBox_Bank.Items.Add(DataFormat.BankOfAmerica);    //!FIX:
-            //comboBox_Bank.Items.Add(DataFormat.CapitolOne360_Rev0);
-            //comboBox_Bank.SelectedItem = DataFormat.CapitolOne360_Rev0;
-
-            //comboBox_Filter.Items.Add(FilterGroup.Food);
-            //comboBox_Filter.Items.Add(FilterGroup.Utility);
-            //comboBox_Filter.Items.Add(FilterGroup.Gas);
-            //comboBox_Filter.SelectedItem = FilterGroup.Food;
         }
 
         private void ParseTransactions_OnChanged(object sender, EventArgs args)
@@ -59,35 +25,6 @@ namespace ParseAndFilterTransactions
             groupBox_FilterTranx.Enabled = (ParseTransactions.AllLoadedTransactions.Count > 0);
             groupBox_SummarizeTranx.Enabled = (ParseTransactions.FilteredTransactions.Count > 0);
             groupBox_Save.Enabled = (ParseTransactions.SummarizedTransactions.Count > 0);
-        }
-
-        private Dictionary<DataFormat, string> m_RootFolders;
-
-        private Dictionary<FilterGroup, List<string>> m_FilterGroupKeywords;
-
-        private void comboBox_Bank_SelectedIndexChanged(object sender, EventArgs e)
-        {   //!FIX: remove
-            //textBox_RootFolder.Text = m_RootFolders[(DataFormat)comboBox_Bank.SelectedItem];
-        }
-
-        private void buttonSelectSourceFile_Click(object sender, EventArgs e)
-        {   //!FIX: remove
-            //openFileDialog1.FileName = textBox_RootFolder.Text;
-
-            //if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            //{
-            //    textBoxFileName.Text = openFileDialog1.FileName;
-            //}
-        }
-
-        private void comboBox_Filter_SelectedIndexChanged(object sender, EventArgs e)
-        {   //!FIX: remove
-            //List<string> list = m_FilterGroupKeywords[(FilterGroup)comboBox_Filter.SelectedItem];
-            //listBoxFilterKeywords.Items.Clear();
-            //foreach (string keyword in list)
-            //{
-            //    listBoxFilterKeywords.Items.Add(keyword);
-            //}
         }
     }
 }
